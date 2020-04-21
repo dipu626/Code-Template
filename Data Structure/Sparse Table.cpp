50908f6aa2a3e6164820e3d7b1dd4447f1716b26
Dipu Kumar Mohanto <63247883+dipu626@users.noreply.github.com>
2020-04-21 11:08:28 +0600

    Add files via upload

---
+++ Data Structure/Sparse Table.cpp
@@ -0,0 +1,28 @@
+template <typename T>
+struct SparseTable
+{
+      int n;
+      vector < vector <T> > Table;
+
+      void init(int N) {
+            n = N;
+            Table.clear();
+            Table.resize(logn, vector <T> (n));
+      }
+      void build(T *arr) {
+            for (int i = 0; i < n; i++) Table[0][i] = arr[i];
+            for (int k = 1; (1 << k) <= n; k++) {
+                  for (int i = 0; i + (1 << (k - 1)) < n; i++) {
+                        int x = Table[k - 1][i];
+                        int y = Table[k - 1][i + (1 << (k - 1))];
+                        Table[k][i] = min(x, y);
+                  }
+            }
+      }
+      T query(int i, int j) {
+            int k = log2(j - i + 1);
+            int x = Table[k][i];
+            int y = Table[k][j + 1 - (1 << k)];
+            return min(x, y);
+      }
+};
