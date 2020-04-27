594473ade817d5ea35168c2efceb54341a311753
Dipu Kumar Mohanto <63247883+dipu626@users.noreply.github.com>
2020-04-27 21:56:23 +0600

    Update SegmentTree2D.cpp

--- Data Structure/Segment Tree/SegmentTree2D.cpp
+++ Data Structure/Segment Tree/SegmentTree2D.cpp
@@ -17,23 +17,12 @@ struct SegmentTree2D {
             column = c;
             st.resize(4 * r, vector <pll> (4 * c));
       }
-      // here we are not changing first 3 arguments. Means, our rows are fixed.
-      // we narrow down to our columns, and generate segment tree for row [r1,r2] (including)
-      // we can pass flag instead of passing two parameters r1 and r2 (thats just optimization)
       void gen_segtree_from_c1_to_c2(int parSegTree, int r1, int r2, int node, int c1, int c2) {
             if (c1 == c2) {
                   if (r1 == r2) {
-                        // means we are working on single row,
-                        // and we can directly generate segment tree for that row = r1;
                         st[parSegTree][node].ff = st[parSegTree][node].ss = grid[r1][c1];
                         return;
                   }
-                  // here, parSegTree      = parent segment tree
-                  // (parSegTree * 2)     = left child segment tree
-                  // (parSegTree * 2 + 1) = right child segment tree
-                  // we build parent segment tree using values of children segment trees
-
-                  // we are creating segment tree which contains sum from row (r1 to r2)
                   if (r1 != r2) {
                         int lchild = parSegTree * 2;
                         int rchild = lchild + 1;
@@ -51,7 +40,6 @@ struct SegmentTree2D {
             st[parSegTree][node].ss = max(st[parSegTree][left].ss, st[parSegTree][right].ss);
       }
       void gen_segtree_from_r1_to_r2(int node, int r1, int r2) {
-            // if we have narrow down to single row
             if (r1 == r2) {
                   gen_segtree_from_c1_to_c2(node, r1, r2, 1, 1, column);
                   return;
@@ -61,7 +49,6 @@ struct SegmentTree2D {
             int mid = (r1 + r2) >> 1;
             gen_segtree_from_r1_to_r2(left, r1, mid);
             gen_segtree_from_r1_to_r2(right, mid+1, r2);
-            // now using upper two segment trees, we create new segment tree
             gen_segtree_from_c1_to_c2(node, r1, r2, 1, 1, column);
       }
       void build() {
