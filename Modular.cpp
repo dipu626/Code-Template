be5e5683afff6097d21a22d75691cf55d4b2fc04
Dipu Kumar Mohanto <63247883+dipu626@users.noreply.github.com>
2020-06-28 17:29:39 +0600

    Update Modular.cpp

--- Modular.cpp
+++ Modular.cpp
@@ -97,40 +97,31 @@ static const long long INF = 1e18;
 struct mi {
       long long v;
       explicit operator long long() const { return v; }
-	mi() { v = 0; }
-	mi(long long _v) {
-		v = (-MOD < _v && _v < MOD) ? _v : _v % MOD;
-		if (v < 0) { v += MOD; }
-	}
-	friend bool operator == (const mi& a, const mi& b) { return a.v == b.v; }
-	friend bool operator != (const mi& a, const mi& b) { return !(a == b); }
-	friend bool operator < (const mi& a, const mi& b) { return a.v < b.v; }
+      mi() { v = 0; }
+      mi(long long _v) { v = (-MOD < _v && _v < MOD) ? _v : _v % MOD; if (v < 0) { v += MOD; } }
 
-	mi& operator += (const mi& m) { if ((v += m.v) >= MOD) { v -= MOD; } return *this; }
-	mi& operator -= (const mi& m) { if ((v -= m.v) < 0) { v += MOD; } return *this; }
-	mi& operator *= (const mi& m) { v = v * m.v % MOD; return *this; }
-	mi& operator /= (const mi& m) { return (*this) *= inv(m); }
-	friend mi pow(mi a, long long p) {
-		mi ans = 1;
-		assert(p >= 0);
-		for (; p; p /= 2, a *= a) {
-                  if (p & 1) {
-                        ans *= a;
-                  }
-		}
-		return ans;
-	}
-	friend mi inv(const mi& a) { assert(a.v != 0); return pow(a, MOD - 2); }
+      friend bool operator == (const mi& a, const mi& b) { return a.v == b.v; }
+      friend bool operator != (const mi& a, const mi& b) { return !(a == b); }
+      friend bool operator < (const mi& a, const mi& b) { return a.v < b.v; }
 
-	mi operator - () const { return mi(-v); }
-	mi& operator ++ () { return *this += 1; }
-	mi& operator -- () { return *this -= 1; }
+      mi& operator += (const mi& m) { if ((v += m.v) >= MOD) { v -= MOD; } return *this; }
+      mi& operator -= (const mi& m) { if ((v -= m.v) < 0) { v += MOD; } return *this; }
+      mi& operator *= (const mi& m) { v = v * m.v % MOD; return *this; }
+      mi& operator /= (const mi& m) { return (*this) *= inv(m); }
+
+      friend mi pow(mi a, long long p) { mi ans = 1; assert(p >= 0); for (; p; p /= 2, a *= a) { if (p & 1) { ans *= a; } } return ans; }
+      friend mi inv(const mi& a)       { assert(a.v != 0); return pow(a, MOD - 2); }
+
+      mi operator - () const { return mi(-v); }
+      mi& operator ++ () { return *this += 1; }
+      mi& operator -- () { return *this -= 1; }
       mi operator ++ (int) { mi temp; temp.v = v++; return temp; }
       mi operator -- (int) { mi temp; temp.v = v--; return temp; }
-	friend mi operator + (mi a, const mi& b) { return a += b; }
-	friend mi operator - (mi a, const mi& b) { return a -= b; }
-	friend mi operator * (mi a, const mi& b) { return a *= b; }
-	friend mi operator / (mi a, const mi& b) { return a /= b; }
+
+      friend mi operator + (mi a, const mi& b) { return a += b; }
+      friend mi operator - (mi a, const mi& b) { return a -= b; }
+      friend mi operator * (mi a, const mi& b) { return a *= b; }
+      friend mi operator / (mi a, const mi& b) { return a /= b; }
 
       friend ostream& operator << (ostream& os, const mi& m) { os << m.v; return os; }
       friend istream& operator >> (istream& is, mi& m) { long long x; is >> x; m.v = x; return is; }
