9898b755b4df5a506482a21540cc0912ced14802
Dipu Kumar Mohanto <63247883+dipu626@users.noreply.github.com>
2020-08-09 15:52:50 +0600

    Update SuffixTree.cpp

--- String/SuffixTree.cpp
+++ String/SuffixTree.cpp
@@ -1,124 +1,200 @@
-struct Node {
-      int l;
-      int r;
-      int par;
-      int link;
-      map <char,int> next;
-      Node(int l = 0, int r = 0, int par = -1)
-            : l(l), r(r), par(par), link(-1) {}
+/*
+      Algorithm : Suffix Tree
+      Tutorial  : https://cp-algorithms.com/string/suffix-tree-ukkonen.html
+                  https://www.geeksforgeeks.org/ukkonens-suffix-tree-construction-part-1/?ref=lbp
+                  https://codeforces.com/blog/entry/16780
+                  
+      Visualization : https://brenden.github.io/ukkonen-animation/
+      
+      Structure Details : 
+                              Root = 0
+                              l    = starting index of the substring
+                              r    = ending index of the substring + 1
+                            
+      Problem   : https://codeforces.com/contest/427/problem/D
+      Statement : Given two strings s1 and s2 consist of lowercase Latin letters, find the smallest (by length) 
+                  common substring p of both s1 and s2, where p is a unique substring in s1 and also in s2. 
+      
+*/
 
-      void clean() {
-            l = 0;
-            r = 0;
-            par = -1;
-            link = -1;
-            next.clear();
-      }
-      int len() {
-            return r - l;
-      }
-      int &get(char c) {
-            if (!next.count(c)) {
-                  next[c] = -1;
+struct SuffixTree {
+      struct Node {
+            int l;
+            int r;
+            int par;
+            int link;
+            map <char, int> child;
+            Node(int l = 0, int r = 0, int par = -1)
+                  : l(l), r(r), par(par), link(-1) {
+                        child.clear();
+                  }
+
+            void clean() {
+                  l = 0;
+                  r = 0;
+                  par = -1;
+                  link = -1;
+                  child.clear();
             }
-            return next[c];
+            int len() {
+                  return r - l;
+            }
+            int &get(char c) {
+                  if (child.count(c) == 0) {
+                        child[c] = -1;
+                  }
+                  return child[c];
+            }
+      };
+
+      struct State {
+            int v;
+            int pos;
+            State() {}
+            State(int v, int pos)
+                  : v(v), pos(pos) {}
+      };
+
+      string s;
+      int n;
+      int SZ;
+      vector <Node> stree;
+      State ptr;
+
+      SuffixTree() {}
+
+      void init(string &str) {
+            s = str;
+            n = str.size();
+            stree.clear(); stree.resize(4 * n + 5);
+      }
+      State go(State st, int l, int r) {
+            while (l < r) {
+                  if (st.pos == stree[st.v].len()) {
+                        st = State(stree[st.v].get(s[l]), 0);
+                        if (st.v == -1) {
+                              return st;
+                        }
+                  }
+                  else {
+                        assert(stree[st.v].l + st.pos >= 0);
+                        if (s[ stree[st.v].l + st.pos ] != s[l]) {
+                              return State(-1, -1);
+                        }
+                        if (r - l < stree[st.v].len() - st.pos) {
+                              return State(st.v, st.pos + r - l);
+                        }
+                        l += stree[st.v].len() - st.pos;
+                        st.pos = stree[st.v].len();
+                  }
+            }
+            return st;
+      }
+      int split(State st) {
+            if (st.pos == stree[st.v].len()) {
+                  return st.v;
+            }
+            if (st.pos == 0) {
+                  return stree[st.v].par;
+            }
+            Node v = stree[st.v];
+            int id = SZ++;
+            stree[id] = Node(v.l, v.l + st.pos, v.par);
+            stree[v.par].get(s[v.l]) = id;
+            stree[id].get(s[v.l + st.pos]) = st.v;
+            assert(st.v >= 0);
+            stree[st.v].par = id;
+            stree[st.v].l += st.pos;
+            return id;
+      }
+      int get_link(int v) {
+            if (stree[v].link != -1) {
+                  return stree[v].link;
+            }
+            if (stree[v].par == -1) {
+                  return 0;
+            }
+            int to = get_link(stree[v].par);
+            return stree[v].link = split(go(State(to, stree[to].len()), stree[v].l + (stree[v].par == 0), stree[v].r));
+      }
+      void tree_extend(int pos) {
+            for(;;) {
+                  State nptr = go(ptr, pos, pos + 1);
+                  if (nptr.v != -1) {
+                        ptr = nptr;
+                        return;
+                  }
+                  int mid = split(ptr);
+                  int leaf = SZ++;
+                  stree[leaf] = Node(pos, n, mid);
+                  stree[mid].get(s[pos]) = leaf;
+                  ptr.v = get_link(mid);
+                  ptr.pos = stree[ptr.v].len();
+                  if (!mid) {
+                        break;
+                  }
+            }
+      }
+      void build_tree() {
+            SZ = 1;
+            ptr = {0, 0};
+            for (int i = 0; i < stree.size(); i++) {
+                        stree[i].clean();
+            }
+            for (int i = 0; i < n; i++) {
+                  tree_extend(i);
+            }
       }
 };
 
-Node t[maxn];
-string s;
-int sz;
-int n;
+SuffixTree st;
+string s1;
+string s2;
+int len1;
+int len2;
 
-struct state {
-      int v, pos;
-      state() {}
-      state(int v, int pos)
-            : v(v), pos(pos) {}
-};
-
-state ptr;
-
-state go(state st, int l, int r) {
-      while (l < r) {
-            if (st.pos == t[st.v].len()) {
-                  st = state(t[st.v].get(s[l]), 0);
-                  if (st.v == -1) {
-                        return st;
+int dfs(int u = 0) {
+      int mini = INF;
+      vector <int> go;
+      for (auto it : st.stree[u].child) {
+            char ch = it.first;
+            int v = it.second;
+            go.push_back(v);
+      }
+      int len = go.size();
+      if (len < 2) {
+            return mini;
+      }
+      if (len == 2) {
+            int pos1 = st.stree[ go[0] ].l;
+            int pos2 = st.stree[ go[1] ].l;
+            if (pos1 > pos2) {
+                  swap(pos1, pos2);
+            }
+            if (st.stree[ go[0] ].r == st.n && st.stree[ go[1] ].r == st.n) {
+                  if (pos1 <= len1 and pos2 > len1) {
+                        return 1;
                   }
-            }
-            else
-            {
-                  assert(t[st.v].l + st.pos >= 0);
-                  if (s[ t[st.v].l + st.pos ] != s[l]) {
-                        return state(-1, -1);
-                  }
-                  if (r - l < t[st.v].len() - st.pos) {
-                        return state(st.v, st.pos + r - l);
-                  }
-                  l += t[st.v].len() - st.pos;
-                  st.pos = t[st.v].len();
+                  return mini;
             }
       }
-      return st;
+      for (int v : go) {
+            mini = min(mini, st.stree[u].len() + dfs(v));
+      }
+      return mini;
 }
 
-int split(state st) {
-      if (st.pos == t[st.v].len()) {
-            return st.v;
+signed main()
+{
+      cin >> s1 >> s2;
+      len1 = s1.size();
+      len2 = s2.size();
+      string str = s1 + "#" + s2 + "$";
+      st.init(str);
+      st.build_tree();
+      int ans = dfs();
+      if (ans == INF) {
+            ans = -1;
       }
-      if (st.pos == 0) {
-            return t[st.v].par;
-      }
-      Node v = t[st.v];
-      int id = sz++;
-      t[id] = Node(v.l, v.l + st.pos, v.par);
-      t[v.par].get(s[v.l]) = id;
-      t[id].get(s[v.l + st.pos]) = st.v;
-      assert(st.v >= 0);
-      t[st.v].par = id;
-      t[st.v].l += st.pos;
-      return id;
+      cout << ans;
 }
-
-int get_link(int v) {
-      if (t[v].link != -1) {
-            return t[v].link;
-      }
-      if (t[v].par == -1) {
-            return 0;
-      }
-      int to = get_link(t[v].par);
-      return t[v].link = split(go(state(to, t[to].len()), t[v].l + (t[v].par == 0), t[v].r));
-}
-
-void tree_extend(int pos) {
-      for(;;) {
-            state nptr = go(ptr, pos, pos + 1);
-            if (nptr.v != -1) {
-                  ptr = nptr;
-                  return;
-            }
-            int mid = split(ptr);
-            int leaf = sz++;
-            t[leaf] = Node(pos, n, mid);
-            t[mid].get(s[pos]) = leaf;
-            ptr.v = get_link(mid);
-            ptr.pos = t[ptr.v].len();
-            if (!mid) {
-                  break;
-            }
-      }
-}
-
-void build_tree() {
-      sz = 1;
-      ptr = {0, 0};
-      for (int i = 0; i < maxn; i++) {
-            t[i].clean();
-      }
-      for (int i = 0; i < n; i++) {
-            tree_extend(i);
-      }
-}
-
